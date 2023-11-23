a8805463b2f13cbe8f0d7b842207cff04eaae7fd
Sannie05 <sannie.olsson@gmail.com>
2023-11-23 11:05:12 +0100

    Graf implementering

--- data_analys.py
+++ data_analys.py
@@ -1,10 +1,13 @@
 import csv
+import matplotlib.pyplot as plt
+import numpy as np
 
 fields = []
 rows = []
 
 data = []
 sum = 0
+
 
 with open('data.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile)
@@ -16,7 +19,27 @@ with open('data.csv', 'r') as csvfile:
 
         data.append(int(row[0]))
 
-for i in data:
-   sum += data[i]
+    for i in data:
+       sum += i
 
-#print(sum / len(data))
+    print(sum / len(data))
+
+    x = []
+    y = []
+
+def heightchart():
+    with open('data.csv', 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
+
+        for row in plots:
+            x.append(row[0])
+            y.append(str(row[0]))
+
+    plt.bar(x, y, color='g', width=0.5, label="Höjd (cm)")
+    plt.xlabel('Tid')
+    plt.ylabel('Höjd (cm)')
+    plt.title('Växt diagram')
+    plt.legend()
+    plt.show()
+
+heightchart()
