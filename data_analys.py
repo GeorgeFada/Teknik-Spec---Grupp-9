b6e28c1c0fa9f2eed790bb0a1ce6b1d8cb1a52cf
Sannie05 <sannie.olsson@gmail.com>
2023-12-07 10:53:48 +0100

    några förbättringar

--- data_analys.py
+++ data_analys.py
@@ -1,6 +1,5 @@
 import csv
 import matplotlib.pyplot as plt
-import numpy as np
 
 fields = []
 rows = []
@@ -8,11 +7,12 @@ rows = []
 data = []
 sum = 0
 
-
 with open('data.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile)
 
     fields = next(csvreader)
+    fields = next(csvreader)
+
 
     for row in csvreader:
         rows.append(row)
@@ -22,12 +22,11 @@ with open('data.csv', 'r') as csvfile:
     for i in data:
        sum += i
 
-    print(sum / len(data))
-
     x = []
     y = []
 
-def heightchart():
+
+def sunhourchartheight():
     with open('data.csv', 'r') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
 
@@ -36,10 +35,28 @@ def heightchart():
             y.append(str(row[0]))
 
     plt.bar(x, y, color='g', width=0.5, label="Höjd (cm)")
-    plt.xlabel('Tid')
+    plt.xlabel('Soltimmar (h)')
     plt.ylabel('Höjd (cm)')
     plt.title('Växt diagram')
     plt.legend()
     plt.show()
 
-heightchart()
+def sunhourchartmass():
+    with open('data.csv', 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
+
+        for row in plots:
+            x.append(row[0])
+            y.append(str(row[3]))
+
+    plt.bar(x, y, color='g', width=0.5, label="Massa (g)")
+    plt.title('Växt diagram')
+    plt.legend()
+    plt.show()
+
+graph_input = str(input('soltimmarhöjd\nsoltimmarmassa\nVilken typ av data vii du se? '))
+if graph_input == 'soltimmarhöjd':
+    sunhourchartheight()
+elif graph_input == 'soltimmarmassa':
+    sunhourchartmass()
+
