e3da12ae6234c5b87b161e5daf3c63968ac5c7a4
Sannie05 <sannie.olsson@gmail.com>
2023-12-14 11:09:27 +0100

    grafer
    Vidarutveckling av grafer med dictionary och som visar två eller fler utdata istället för en

--- data_analys.py
+++ data_analys.py
@@ -1,62 +1,67 @@
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
-fields = []
-rows = []
-
-data = []
-sum = 0
+import csv
 
 with open('data.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile)
 
-    fields = next(csvreader)
-    fields = next(csvreader)
+    indata_index = 0
+    utdata_index = 0
 
+    # Spara indexer för var indatan respektive utdatan börjar
+    for row in csvreader:
+        # ksk ta bort extra kolumner emellan Indata och utdata som inte behövs.
+        # Anta inte att det heter indata eller utdata ska kunna vara vad som ksk.
+        indata_index = row.index("Indata")
+        utdata_index = row.index("Utdata")
+        break
+
+    data_titles = next(csvreader)
+
+    data_dict = dict(Indata=[], Utdata=[])  # Spara allt i en dictionary
+
+    # Lägg in data titlarna först
+    for i in range(indata_index, utdata_index - 1):
+        array = []
+        array.append(data_titles[i])
+        array.append([])
+        data_dict["Indata"].append(array)
+    for i in range(utdata_index, len(data_titles)):
+        array = []
+        array.append(data_titles[i])
+        array.append([])
+        data_dict["Utdata"].append(array)
 
     for row in csvreader:
-        rows.append(row)
+        for i in range(indata_index, utdata_index - 1):
+            data_dict["Indata"][i][1].append(row[i])
+        num = 0
+        for i in range(utdata_index, len(data_titles)):
+            data_dict["Utdata"][num][1].append(row[i])
+            num += 1
+    # print(data_dict)
 
-        data.append(int(row[0]))
+    # Printa ut för varje rad i csv filen de olika kolumnernas värde, vad för typ av värde, samt om det är indata/utdata
+    # <radnummer> Indata: Titel: <värde> (osv), Utdata: Titel:
+    for i in range(len(data_dict["Utdata"][0][1])):
+        print(str(i + 1) + ".")
+        dict_indata = data_dict["Indata"]
+        dict_utdata = data_dict["Utdata"]
+        print("Indata:")
+        for item in dict_indata:
+            print(item[0] + ":", item[1][i])
+        print("Utdata:")
+        for item in dict_utdata:
+            print(item[0] + ":", item[1][i])
+        print("")  # Nyrad
 
-    for i in data:
-       sum += i
 
-    x = []
-    y = []
+plt.xlabel(dict_indata[0][0])
+labels = [dict_utdata[0][0], dict_utdata[1][0]]
+plt.ylabel(labels)
 
-
-def sunhourchartheight():
-    with open('data.csv', 'r') as csvfile:
-        plots = csv.reader(csvfile, delimiter=',')
-
-        for row in plots:
-            x.append(row[0])
-            y.append(str(row[0]))
-
-    plt.bar(x, y, color='g', width=0.5, label="Höjd (cm)")
-    plt.xlabel('Soltimmar (h)')
-    plt.ylabel('Höjd (cm)')
-    plt.title('Växt diagram')
-    plt.legend()
-    plt.show()
-
-def sunhourchartmass():
-    with open('data.csv', 'r') as csvfile:
-        plots = csv.reader(csvfile, delimiter=',')
-
-        for row in plots:
-            x.append(row[0])
-            y.append(str(row[3]))
-
-    plt.bar(x, y, color='g', width=0.5, label="Massa (g)")
-    plt.title('Växt diagram')
-    plt.legend()
-    plt.show()
-
-graph_input = str(input('soltimmarhöjd\nsoltimmarmassa\nVilken typ av data vii du se? '))
-if graph_input == 'soltimmarhöjd':
-    sunhourchartheight()
-elif graph_input == 'soltimmarmassa':
-    sunhourchartmass()
-
+print(dict_utdata)
+plt.plot(dict_indata[0][1], dict_utdata[0][1], dict_utdata[1][1])
+plt.show()
