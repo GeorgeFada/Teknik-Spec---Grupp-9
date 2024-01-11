501253a99f97360b0fd62dadbebe383a06cf469a
Ivar <ivar@E7440.localdomain>
2024-01-11 11:29:23 +0100

    kommentering av kod för förtydligande, cleanup

--- data_analys.py
+++ data_analys.py
@@ -1,5 +1,5 @@
 import csv
-import argparse
+import argparse # ge filen till programmet via cmdline
 import os.path
 
 parser = argparse.ArgumentParser()
@@ -7,15 +7,17 @@ parser.add_argument("filename", nargs='?', help="the source csv file containing 
 args = parser.parse_args()
 
 def check_extension(filename):
-    filename_extension = os.path.splitext(filename)[1]
+    filename_extension = os.path.splitext(filename)[1] # Få filändelse
     if filename_extension != ".csv":
         print("The file", "\"" + filename + "\"", "is not a csv file.")
         parser.print_help()
         exit()
 
+# Hantera hur programmet tar emot filen.
+# Kolla först om man givit filen som argument på cmdline, annars ska programmet köra drag and drop
 if args.filename:
     #todo kolla om extension är .csv
-    if not os.path.exists(args.filename): #Om inte hittar fil:
+    if not os.path.exists(args.filename): # Om filen inte existerar
         print("Could not open", "\"" + os.path.basename(args.filename) + "\"" + ", no such file or directory.")
         parser.print_help()
         exit()
@@ -25,30 +27,31 @@ if args.filename:
 else:
     pass
     exit("Drag")
-    #HÄR KÖRS DRAG AND DROP
-    #csv_file = file
+    # HÄR KÖRS DRAG AND DROP
+    # csv_file = file
 
 with open(csv_file, 'r') as csvfile:
     csvreader = csv.reader(csvfile)
 
-    indata_index = 0 
-    utdata_index = 0 
-
-    #Spara indexer för var indatan respektive utdatan börjar
-    # print(csvreader)
-    # exit()
+    # Spara indexer för var indatan respektive utdatan börjar
     for row in csvreader:
-        #ksk ta bort extra kolumner emellan Indata och utdata som inte behövs.
-        #Anta inte att det heter indata eller utdata ska kunna vara vad som ksk.
+        # Anta inte att det heter indata eller utdata ska kunna vara vad som ksk.
         indata_index = row.index("Indata")
         utdata_index = row.index("Utdata")
         break
 
+    # Spara raden i csv-filen med alla titlar i listan data_titles.
+    # Mellanrum mellan titlarna representerar byte från indata- respektive utdata titlar.
     data_titles = next(csvreader)
     
-    data_dict = dict(Indata = [], Utdata = []) #Spara allt i en dictionary
+    # Allting i csv filen sparas i en dictionary för att lätt hantera datan.
+    # Den är utformad som:
+    # {'Indata': [['<data_titel>', ['<värde>', '<värde>']], []],
+    #  'Utdata': [['<data_titel>', ['<värde>', '<värde>']], []]
+    # }
+    data_dict = dict(Indata = [], Utdata = [])
 
-    #Lägg in data titlarna först
+    # Lägg in data titlarna först
     for i in range(indata_index, utdata_index-1):
         array = []
         array.append(data_titles[i])
@@ -67,10 +70,8 @@ with open(csv_file, 'r') as csvfile:
         for i in range(utdata_index, len(data_titles)):
             data_dict["Utdata"][num][1].append(row[i])
             num += 1
-    # print(data_dict)
-    # exit()
 
-    #Printa ut för varje rad i csv filen de olika kolumnernas värde, vad för typ av värde, samt om det är indata/utdata
+    # Printa ut för varje rad i csv filen de olika kolumnernas värde, vad för typ av värde, samt om det är indata/utdata
     # <radnummer> Indata: Titel: <värde> (osv), Utdata: Titel:
     # for i in range(len(data_dict["Utdata"][0][1])):
     dict_indata = data_dict["Indata"]
@@ -83,4 +84,4 @@ with open(csv_file, 'r') as csvfile:
         print("Utdata:")
         for item in dict_utdata:
             print(item[0] + ":", item[1][i])
-        print("") #Nyrad
+        print("")
